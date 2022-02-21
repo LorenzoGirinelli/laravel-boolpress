c9cac2e66ef9862925266574277393b7a4ba744c
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-02-21 17:47:02 +0100

    finito

---
+++ resources/js/views/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>ciao sono vue</h1>
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue';
+
+export default {
+    name: 'App'
+}
+</script>i
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::middleware('auth')
@@ -26,3 +22,7 @@ Route::middleware('auth')
     ->group(function() {
         Route::get('/','HomeController@index')->name('home');
     });
+
+Route::get("(any?)", function() {
+    return view('guest.home');
+})->where('any','.*');
