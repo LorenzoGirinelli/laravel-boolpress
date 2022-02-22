b9a686d0e1b70355a2a61928b056816d460c8df0
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-02-22 17:53:24 +0100

    posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <section>
+        <div class="container">
+            <h1>Lista dei post</h1>
+
+            <div class="row row-cols-3">
+                @foreach ($posts as $post)
+                    {{-- Single post --}}
+                    <div class="col">
+                        <div class="card mt-2">
+                            {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                            <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p class="card-text">{{ Str::substr($post->content, 0, 70) }}...</p>
+                            <a href="#" class="btn btn-primary">Leggi il post</a>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Single post --}}
+                @endforeach
+            </div>
+        </div>
+    </section>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function() {
         Route::get('/','HomeController@index')->name('home');
+        Route::resource('posts','PostController');
     });
 
 Route::get('{any?}', function() {
